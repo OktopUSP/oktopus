64f0d5a08872797f9587c48ff35ca9bc3d3d9a2c
leandrofars <leandrofars@gmail.com>
2023-10-27 01:47:53 -0300

    feat(mtp): stomp server

---
+++ backend/services/stomp/cmd/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	stomp_server "github.com/go-stomp/stomp/server"
+	"github.com/joho/godotenv"
+)
+
+type Credentials struct {
+	Login  string
+	Passwd string
+}
+
+func (c *Credentials) Authenticate(login, passwd string) bool {
+
+	if c.Login == "" && c.Passwd == "" {
+		log.Println("NEW CLIENT AUTH: Server is running with no auth")
+		return true
+	}
+
+	if login != c.Login || passwd != c.Passwd {
+		log.Println("NEW CLIENT AUTH: Invalid Credentials")
+		return false
+	}
+	log.Println("NEW CLIENT AUTH: OK")
+	return true
+}
+
+func main() {
+
+	err := godotenv.Load()
+	localEnv := ".env.local"
+	if _, err := os.Stat(localEnv); err == nil {
+		_ = godotenv.Overload(localEnv)
+		log.Println("Loaded variables from '.env.local'")
+	} else {
+		log.Println("Loaded variables from '.env'")
+	}
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	creds := Credentials{
+		Login:  os.Getenv("STOMP_USERNAME"),
+		Passwd: os.Getenv("STOMP_PASSWORD"),
+	}
+
+	l, err := net.Listen("tcp", stomp_server.DefaultAddr)
+	if err != nil {
+		log.Println("Error to open tcp port: ", err)
+	}
+
+	s := stomp_server.Server{
+		Addr:          stomp_server.DefaultAddr,
+		HeartBeat:     stomp_server.DefaultHeartBeat,
+		Authenticator: &creds,
+	}
+
+	log.Println("Started STOMP server at port", s.Addr)
+	err = s.Serve(l)
+	if err != nil {
+		log.Println("Error to start stomp server: ", err)
+	}
+}
---
+++ backend/services/stomp/go.mod
@@ -0,0 +1,8 @@
+module github.com/leandrofars/stomp
+
+go 1.21.3
+
+require (
+	github.com/go-stomp/stomp v2.1.4+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
