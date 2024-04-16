be5a69f37cb9d934a49e85f8b8243eea0b22217a
Roger <roger.sacchelli@gmail.com>
2024-04-16 23:59:55 +0000

    Config Map for HAProxy Ingress Controller

---
+++ deploy/kubernetes/README.md
@@ -0,0 +1,16 @@
+# Oktopus Kubernetes
+
+## Requirements
+
+### Standalone Installation
+
+Node:
+* 8 vCPUs
+* 8 GB RAM
+
+# Installation
+
+
+## MongoBD
+
+ 
--- frontend/build/entrypoint.sh
+++ frontend/build/entrypoint.sh
@@ -3,14 +3,14 @@ set -Ex
 
 function apply_path {
 
-    echo "Check that we have NEXT_PUBLIC_REST_ENPOINT vars"
-    test -n "$NEXT_PUBLIC_REST_ENPOINT"
+    echo "Check that we have NEXT_PUBLIC_REST_ENDPOINT vars"
+    test -n "$NEXT_PUBLIC_REST_ENDPOINT"
 
-    echo "Check that we have NEXT_PUBLIC_WS_ENPOINT vars"
-    test -n "$NEXT_PUBLIC_WS_ENPOINT"
+    echo "Check that we have NEXT_PUBLIC_WS_ENDPOINT vars"
+    test -n "$NEXT_PUBLIC_WS_ENDPOINT"
 
-    find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#REST_API_URL#$NEXT_PUBLIC_REST_ENPOINT#g"
-    find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#WS_URL#$NEXT_PUBLIC_WS_ENPOINT#g"
+    find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#REST_API_URL#$NEXT_PUBLIC_REST_ENDPOINT#g"
+    find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#WS_URL#$NEXT_PUBLIC_WS_ENDPOINT#g"
 }
 
 apply_path
